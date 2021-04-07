1ea68303413d689d85f1f57028f16835fb4fd060
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-04-07 13:41:19 +0200

    Use electron logger

--- src/app/shared/extractor.ts
+++ src/app/shared/extractor.ts
@@ -5,6 +5,7 @@ import { CookieJar, JSDOM } from 'jsdom';
 
 import { PSAMovieRelease, PSAShowRelease } from '../model/PSARelease.interface';
 import { serializeForm, btoa } from './utils';
+import LOG from 'electron-log';
 
 class PSAExtractor {
 	private ax: AxiosInstance;
@@ -63,7 +64,7 @@ class PSAExtractor {
 				new URL(link).origin
 			);
 
-			console.log(`Trying ${this.ouoIndices[i]}`);
+			LOG.info(`Trying ${this.ouoIndices[i]}`);
 
 			const { headers } = await this.ax.get(link, {
 				jar: this.cj,
@@ -91,7 +92,7 @@ class PSAExtractor {
 		// 2. Find the first form and POST it
 		const formCaptcha = doc.querySelector<HTMLFormElement>('#form-captcha');
 		if (!formCaptcha) {
-			console.log('No captcha form!');
+			LOG.info('No captcha form!');
 			return [];
 		}
 		doc = await this.postForm(formCaptcha);
@@ -99,7 +100,7 @@ class PSAExtractor {
 		// 3. Find the second form and POST it
 		const formGo = doc.querySelector<HTMLFormElement>('#form-go');
 		if (!formGo) {
-			console.log('No go form!');
+			LOG.info('No go form!');
 			return [];
 		}
 		doc = await this.postForm(formGo);
@@ -121,7 +122,7 @@ class PSAExtractor {
 	}
 
 	async add(release: PSAShowRelease | PSAMovieRelease): Promise<string[]> {
-		console.log(
+		LOG.info(
 			`Adding release ${release.name} with ${release.exitLinks.length} link(s)`
 		);
 
