fc6eba3f12f20f9a72cd4bb5136506e901f580d1
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-07-18 22:25:53 +0200

    Add stfly type

--- src/app/shared/model.interface.ts
+++ src/app/shared/model.interface.ts
@@ -2,3 +2,16 @@ export interface IPCData {
 	command: string;
 	[key: string]: any;
 }
+
+export type StflyResult = Yes | No;
+
+export interface Yes {
+	success: true;
+	message: string;
+	url: string;
+}
+
+export interface No {
+	success: false;
+	message: string;
+}
