86aa9aa9e769a44fd2f212d2f328e3e9594d6925
Chris Rebbelin <cr.rebbelin@gmail.com>
2021-07-18 22:25:40 +0200

    Add more utils

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -16,8 +16,19 @@ export function $$(sel: string, doc: Document | Element): HTMLElement[] {
 export function serializeForm(form: HTMLFormElement): string {
 	return Array.from(
 		form.querySelectorAll('input'),
-		(i) => `${i.name}=${i.value}`
+		(i) => `${i.name}=${encodeURIComponent(i.value)}`
 	).join('&');
+}
+
+export function delay(timeInSec: number): Promise<void> {
+	return new Promise((resolve) => setTimeout(resolve, timeInSec * 1000));
+}
+
+// https://stackoverflow.com/a/49966753
+export function urljoin(baseURL: string, relativeURL?: string): string {
+	return relativeURL
+		? baseURL.replace(/\/+$/, '') + '/' + relativeURL.replace(/^\/+/, '')
+		: baseURL;
 }
 
 export function btoa(data: string | number): string {
